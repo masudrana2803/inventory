ba1a7ad436bdd041607d62c527d00a1e501831e0
masudrana2803 <masudrana2803@gmail.com>
2025-08-08 02:18:34 +0600

    inventory

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ function Product(name,quantity,type,stock){
 
 const inventoryItems=document.getElementById('inventoryItems');
 const inventoryList=document.getElementById('inventoryList')
-const itemName=document.getElementById('itemName')
-const itemQty=document.getElementById('itemQty')
-const itemType=document.getElementById('itemType')
-const itemStock=document.getElementById('itemStock')
+const itemName=document.getElementById('item')
+const itemQty=document.getElementById('quantity')
+const itemType=document.getElementById('Type')
+const itemStock=document.getElementById('stock')
 
 let Products=[]
 
@@ -24,37 +24,37 @@ function renderinventoryList(){
     }
 
     Products.forEach((Products, idx) => {
-        const inventoryList = document.createElement('div')
-        inventoryList.classList.add('inventoryList')
-        inventoryList.innerHTML=`
-        <p>Item name: ${product.name}</p>
-            <p>Item Quantity: ${product.quantity}</p>
-                <p>Item type: ${product.type}</p>
-                <p>Item avialability: ${product.stock}</p>
+        const itemDiv = document.createElement('div')
+        itemDiv.classList.add('inventoryList')
+        itemDiv.innerHTML=`
+        <p>Item name: ${Products.name}</p>
+            <p>Item Quantity: ${Products.quantity}</p>
+                <p>Item type: ${Products.type}</p>
+                <p>Item avialability: ${Products.stock}</p>
         `;
-
+        inventoryList.appendChild(itemDiv);
 
     })
 }
 
     inventoryItems.addEventListener('submit', function(e) {
         e.preventDefault();
+        console.log('ha clicked')
 
         const newProduct = new Product(
-            itemName.value.trim(),
-            itemQty.value.trim(),
-            itemType.value.trim(),
-            itemStock.value.trim(),
-
-        );
-
-        if (editIndex !== null) {
-            Products[editIndex] = newProduct;
-            editIndex = null;
-            inventoryItems.querySelector('.btn').textContent = 'Add Student';
-        } else {
-            Products.push(newProduct);
-        }
+        itemName.value.trim(),
+        itemQty.value.trim(),
+        itemType.value.trim(),
+        itemStock.value.trim(),
+    );
+        Products.push(newProduct);
+        // if (editIndex !== null) {
+        //     Products[editIndex] = newProduct;
+        //     editIndex = null;
+        //     inventoryItems.querySelector('.btn').textContent = 'Add Student';
+        // } else {
+        //     Products.push(newProduct);
+        // }
 
         renderinventoryList();
         inventoryItems.reset();
